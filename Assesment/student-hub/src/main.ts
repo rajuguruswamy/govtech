0d9237c680289ba900bbe0be3014da45e0f31a71
raju_guruswamy <raju_guruswamy@tech.gov.sg>
2024-05-05 17:50:27 +0800

    added swagger

--- Assesment/student-hub/src/main.ts
+++ Assesment/student-hub/src/main.ts
@@ -4,12 +4,26 @@ import { Logger } from '@nestjs/common';
 import { ValidationPipe } from '@nestjs/common';
 import { configService } from './config/config.service';
 import * as dotenv from 'dotenv';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 dotenv.config();
 
 async function bootstrap() {
   const logger = new Logger();
 
   const app = await NestFactory.create(AppModule);
+
+  if (!configService.isProduction()) {
+    const document = SwaggerModule.createDocument(
+      app,
+      new DocumentBuilder()
+        .setTitle('Student Hub API')
+        .setDescription('Student Hub  API')
+        .setVersion('1.0')
+        .build(),
+    );
+
+    SwaggerModule.setup('api', app, document);
+  }
 
   // Enable global validation pipe
   app.useGlobalPipes(new ValidationPipe());
