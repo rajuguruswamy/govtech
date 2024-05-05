f1e083c95de6515b9f9810935d198396dfac6952
raju_guruswamy <raju_guruswamy@tech.gov.sg>
2024-05-05 15:48:04 +0800

    removed docker compose file

---
+++ Assesment/student-hub/src/scripts/setenv_localpostgres.sh
@@ -0,0 +1,9 @@
+
+export DATABASE_HOSTNAME=localhost
+export DATABASE_PORT=5432
+export DATABASE_USERNAME=postgres
+export DATABASE_PASSWORD=mysecretpassword
+export DATABASE_NAME=student_hub
+export NODE_PORT=3000
+export NODE_ENV=DEV
+
